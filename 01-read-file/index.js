fdd004f1a472e96ccb516f7facdab937ad4f51cb
SashaRPG <uasasharpg@gmail.com>
2022-05-20 01:15:52 +0300

    fix: added comments on first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,15 +1,20 @@
+//modules import
 const fs = require('fs');
 const path = './01-read-file/text.txt';
 const { stdout } = process;
 
+//reading stream
 const fileInsides = fs.createReadStream(path, 'utf-8');
 
+/* if reading was succesfull, open a file and write data to console,
+destroy stream afterwards */
 fileInsides.on('readable', ()=>{
     let data = fileInsides.read();
     stdout.write(data);
     fileInsides.destroy();
 });
 
+//error handler
 fileInsides.on('error', (err)=>{
     stdout.write(err.message);
 });
